2788fa841692e58881e01dffd01d75d96944f16d
m32 <mak@trisoft.com.pl>
2018-09-14 22:24:11 +0200

    openssl interop

--- examples/smime-decrypt.py
+++ examples/smime-decrypt.py
@@ -7,10 +7,19 @@ from endesive import email
 
 def main():
     key = asymmetric.load_private_key(open('demo2_user1.key.pem', 'rb').read(), '1234')
-    datae = open('smime-encrypted.txt', 'rt', encoding='utf-8').read()
-    datad = email.decrypt(datae, key)
-    datad = datad.decode('utf-8')
-    open('smime-decrypted.txt', 'wt', encoding='utf-8').write(datad)
+    for fname in (
+            'smime-ssl-encrypted.txt',
+            'smime-encrypted.txt',
+    ):
+        print('*' * 20, fname)
+        try:
+            datae = open(fname, 'rt', encoding='utf-8').read()
+        except FileNotFoundError:
+            print('no such file')
+            continue
+        datad = email.decrypt(datae, key)
+        datad = datad.decode('utf-8')
+        open(fname.replace('encrypted', 'decrypted'), 'wt', encoding='utf-8').write(datad)
 
 
 main()
