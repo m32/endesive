7b6beecd13149c0fcc4cf80bbe38e57bb5774f9d
m32 <mak@trisoft.com.pl>
2018-10-22 09:27:24 +0200

    finding first page in outline mode

--- endesive/pdf/cms.py
+++ endesive/pdf/cms.py
@@ -49,10 +49,13 @@ class SignedData(object):
         root = document.xrefs[0].trailer['Root'].objid
         size = document.xrefs[0].trailer['Size']
         page = None
-        for i in document.catalog['OpenAction']:
-            if isinstance(i, PDFObjRef):
-                page = i.objid
-                break
+        if document.catalog['PageMode'] == 'OneColumn':
+            for i in document.catalog['OpenAction']:
+                if isinstance(i, PDFObjRef):
+                    page = i.objid
+                    break
+        else:
+            page = document.getobj(document.catalog['Pages'].objid)['Kids'][0].objid
         infodata = self.getdata(pdfdata1, info, prev, document).strip()
         rootdata = self.getdata(pdfdata1, root, prev, document).strip()
         pagedata = self.getdata(pdfdata1, page, prev, document).strip()
