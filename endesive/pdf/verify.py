75e828ef25a8fcbe4445e23ae8725cefa8200a82
m32 <grzegorz.makarewicz@gmail.com>
2024-09-16 16:41:54 +0200

    more checks

--- endesive/pdf/verify.py
+++ endesive/pdf/verify.py
@@ -10,6 +10,7 @@ def verify(pdfdata, certs=None, systemCertsPath=None):
         stop = pdfdata.find(b"]", start)
         assert n != -1 and start != -1 and stop != -1
         br = [int(i, 10) for i in pdfdata[start + 1 : stop].split()]
+        assert pdfdata[br[1]] == 60 and pdfdata[br[2]-1] == 62
         contents = pdfdata[br[0] + br[1] + 1 : br[2] - 1]
         bcontents = bytes.fromhex(contents.decode("utf8"))
         data1 = pdfdata[br[0] : br[0] + br[1]]
