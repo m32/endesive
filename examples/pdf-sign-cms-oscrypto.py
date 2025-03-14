e0f3ededa5f7cfb40e47e4d567c0f0cceaddd9b0
m32 <grzegorz.makarewicz@gmail.com>
2025-03-14 22:37:26 +0100

    signing with other crypto libraries as HSM module

--- examples/pdf-sign-cms-oscrypto.py
+++ examples/pdf-sign-cms-oscrypto.py
@@ -2,11 +2,31 @@
 # *-* coding: utf-8 *-*
 import sys
 import datetime
-from oscrypto import keys
-from endesive import pdf
+from oscrypto import keys, asymmetric
+from endesive import hsm, pdf
 
 #import logging
 #logging.basicConfig(level=logging.DEBUG)
+
+class OscryptoHSM(hsm.BaseHSM):
+    def __init__(self, key, cert, certs, pss):
+        self.key = key
+        self.cert = cert
+        self.certs = certs
+        self.pss = pss
+
+    def certificate(self):
+        return 1, self.cert
+
+    def sign(self, keyid, tosign, mech):
+        key = asymmetric.load_private_key(self.key)
+        if self.pss:
+            signed_value_signature = asymmetric.rsa_pss_sign(key, tosign, mech.lower())
+        else:
+            signed_value_signature = asymmetric.rsa_pkcs1v15_sign(
+                key, tosign, mech.lower()
+            )
+        return signed_value_signature
 
 def main():
     date = datetime.datetime.utcnow() - datetime.timedelta(hours=12)
@@ -25,16 +45,21 @@ def main():
     }
     with open('ca/demo2_user1.p12', 'rb') as f:
         key, cert, certe = keys.parse_pkcs12(f.read(), b'1234')
-    fname = 'pdf.pdf'
+
     if len (sys.argv) > 1:
         fname = sys.argv[1]
+    else:
+        fname = 'pdf.pdf'
+
     datau = open(fname, 'rb').read()
     datas = pdf.cms.sign(datau, dct,
-        key,
+        None,
         cert,
         certe,
-        'sha256'
+        'sha256',
+        hsm=OscryptoHSM(key, cert, certe, False),
     )
+
     fname = fname.replace('.pdf', '-signed-cms-oscrypto.pdf')
     with open(fname, 'wb') as fp:
         fp.write(datau)
